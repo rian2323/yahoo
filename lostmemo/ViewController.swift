13b03e63f6b889403476b1aaaae20b593a632ec4
riannn0123 <g14910he@gm.tsuda.ac.jp>
2016-09-01 15:17:50 +0900

    Initial Commit

---
+++ lostmemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lostmemo
+//
+//  Created by 波多野　瑛子 on 2016/09/01.
+//  Copyright © 2016年 波多野　瑛子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
