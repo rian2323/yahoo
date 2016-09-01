cae6efe6a31d5e133feb496e68172b850d542474
annap31q <g14919ka@gm.tsuda.ac.jp>
2016-09-01 16:47:02 +0900

    地図の表示

--- lostmemo/ViewController.swift
+++ lostmemo/ViewController.swift
@@ -9,12 +9,52 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMap: MKMapView!
+    @IBOutlet weak var toolBar: UISegmentedControl!
+    
+    var defaultColor:UIColor!
+    
+    @IBAction func gotoSpot(sender: AnyObject) {
+        let lat = 35.454954
+        let lng = 139.6313859
+        let center = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)  // 1 degree means 111km
+        let theRegion = MKCoordinateRegion(center:center, span:span)
+        myMap.setRegion(theRegion,animated: true)
+    }
+    @IBAction func changeMapType(sender: AnyObject) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            myMap.mapType = .Standard
+            myMap.camera.pitch = 0.0
+            toolBar.tintColor = defaultColor
+            toolBar.alpha = 1.0
+        case 1:
+            myMap.mapType = .Satellite
+            toolBar.tintColor = UIColor.whiteColor()
+            toolBar.alpha = 0.8
+        case 2:
+            myMap.mapType = .Hybrid
+            toolBar.tintColor = UIColor.whiteColor()
+            toolBar.alpha = 0.8
+        case 3:
+            myMap.mapType = .Standard
+            toolBar.tintColor = defaultColor
+            toolBar.alpha = 1.0
+            myMap.camera.pitch = 70
+            myMap.camera.altitude = 1000
+        default:
+            break
+        }
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        defaultColor = toolBar.tintColor
+        myMap.showsScale = true
     }
 
     override func didReceiveMemoryWarning() {
